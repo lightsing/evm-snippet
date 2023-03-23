4d3f06c30918e5f3efb43509d7e26b63cfc474c9
lightsing <light.tsing@gmail.com>
2023-03-23 21:49:28 +0800

    add dialogs

---
+++ src/components/addressDialog.tsx
@@ -0,0 +1,71 @@
+import {Button, Dialog, DialogTitle, IconButton, TextField} from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import React from "react";
+
+interface AddressDialogProps {
+  open: boolean,
+  initialValue: string,
+  onSave: (_: string) => void,
+  clearOnSave?: boolean,
+  onClose: () => void,
+}
+
+export interface DialogTitleProps {
+  onClose: () => void;
+}
+
+const AddressDialogTitle = (props: DialogTitleProps) => {
+  const { onClose } = props;
+
+  return (
+    <DialogTitle
+      sx={{m: 0, p: 2}}
+    >
+      Address
+      <IconButton
+        aria-label="close"
+        onClick={onClose}
+        sx={{
+          position: 'absolute',
+          right: 8,
+          top: 8,
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
+    </DialogTitle>
+  )
+}
+
+export const AddressDialog = ({ open, initialValue, onSave, clearOnSave, onClose}: AddressDialogProps) => {
+  const [dialogValue, setDialogValue] = React.useState(initialValue);
+
+  return (
+    <Dialog open={open} onClose={onClose}
+            fullWidth={true}
+            maxWidth="sm"
+    >
+      <AddressDialogTitle onClose={onClose} />
+      <TextField
+        autoFocus
+        margin="normal"
+        label="Address"
+        variant="outlined"
+        value={dialogValue}
+        onChange={e => setDialogValue(e.target.value)}
+        inputProps={{sx: {fontFamily: '"Fira code", "Fira Mono", monospace'}}}
+      />
+      <Button variant="contained" color="primary" onClick={
+        () => {
+          onSave(dialogValue);
+          onClose();
+          if (clearOnSave) {
+            setDialogValue('');
+          }
+        }
+      }>
+        Save
+      </Button>
+    </Dialog>
+  )
+}
--- src/components/addresses.tsx
+++ src/components/addresses.tsx
@@ -1,38 +1,50 @@
-import {Button, List, ListItem, ListItemButton, ListItemText, Stack} from "@mui/material";
-import EditIcon from "@mui/icons-material/Edit";
-import FileOpenIcon from "@mui/icons-material/FileOpen";
-import DeleteIcon from "@mui/icons-material/Delete";
+import {Button, List, Stack} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import React from "react";
+import {AddressItem} from "./address";
+import {AddressDialog} from "./addressDialog";
 
 
 export const Addresses = () => {
-  const [addresses, setAddresses] = React.useState(["0x000000000000000000000000000000000cafe001"]);
+  const [addresses, setAddresses] = React.useState<readonly string[]>(["0x000000000000000000000000000000000cafe001"]);
+
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+
   return (
     <Stack>
       <List>
         {addresses.map((address, index) => (
-          <ListItem
-          >
-            <ListItemText
-              primary={address}
-            />
-            <ListItemButton>
-              <EditIcon />
-            </ListItemButton>
-            <ListItemButton>
-              <FileOpenIcon />
-            </ListItemButton>
-            <ListItemButton>
-              <DeleteIcon />
-            </ListItemButton>
-
-          </ListItem>
+          <AddressItem
+            index={index}
+            address={address}
+            onEdit={
+              (newVal) => {
+                setAddresses((old) => [...old.slice(0, index), newVal, ...old.slice(index + 1)]);
+              }
+            }
+            onDelete={
+              () => {
+                setAddresses(old => old.slice(0, index).concat(old.slice(index + 1)))
+              }
+            }
+          />
         ))}
       </List>
-      <Button variant="outlined" startIcon={<AddIcon />}>
+      <Button variant="outlined" startIcon={<AddIcon />} onClick={() => setDialogOpen(true)}>
         Add New Address
       </Button>
+      <AddressDialog
+        open={dialogOpen}
+        initialValue=""
+        onSave={
+          (address) => {
+            setAddresses(old => [...old, address]);
+            setDialogOpen(false);
+          }
+        }
+        onClose={() => setDialogOpen(false)}
+        clearOnSave
+      />
     </Stack>
   )
 }
