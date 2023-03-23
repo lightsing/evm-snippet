3144e39c7b5bbfe95c5dd98732dc5b169c405dcf
lightsing <light.tsing@gmail.com>
2023-03-23 15:30:43 +0800

    add mui

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import "@fontsource/fira-code";
+import "@fontsource/fira-mono";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
